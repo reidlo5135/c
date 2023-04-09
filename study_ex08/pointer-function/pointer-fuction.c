7b881ca07a3fcfa2a9d6021726e81bc31e4d249d
reidlo5135 <201840103@email.daelim.ac.kr>
2023-04-09 15:28:12 +0900

    refactor : modify pointer-function.c

--- study_ex08/pointer-function/pointer-fuction.c
+++ study_ex08/pointer-function/pointer-fuction.c
@@ -34,21 +34,21 @@ void test05(int * ptr1, int * ptr2) {
 };
 
 int main(void) {
-    // int arr[3] = {1, 2, 3};
-    // test03(arr, sizeof(arr) / sizeof(int), 1);
-    // test02(arr, sizeof(arr) / sizeof(int));
+    int arr[3] = {1, 2, 3};
+    test03(arr, sizeof(arr) / sizeof(int), 1);
+    test02(arr, sizeof(arr) / sizeof(int));
 
-    // test03(arr, sizeof(arr) / sizeof(int), 2);
-    // test02(arr, sizeof(arr) / sizeof(int));
+    test03(arr, sizeof(arr) / sizeof(int), 2);
+    test02(arr, sizeof(arr) / sizeof(int));
 
-    // test03(arr, sizeof(arr) / sizeof(int), 3);
-    // test02(arr, sizeof(arr) / sizeof(int));
+    test03(arr, sizeof(arr) / sizeof(int), 3);
+    test02(arr, sizeof(arr) / sizeof(int));
 
     int num1 = 10;
     int num2 = 20;
     printf("n1 n2 : %d %d \n", num1, num2);
 
-    // test04(num1, num2);
+    test04(num1, num2);
     test05(&num1, &num2);
     printf("num1 num2 : %d %d \n", num1, num2);
     return 0;
