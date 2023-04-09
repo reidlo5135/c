2a6bf0d56f725fe0752a6d2ff9742492811e7594
reidlo5135 <201840103@email.daelim.ac.kr>
2023-04-09 15:27:27 +0900

    feat : add pointer-function

--- study_ex08/pointer-function/include/pointer-function.h
+++ study_ex08/pointer-function/include/pointer-function.h
@@ -3,3 +3,5 @@
 void test01(int * param, int len);
 void test02(int * param, int len);
 void test03(int * param, int len, int add);
+void test04(int n1, int n2);
+void test05(int * ptr1, int * ptr2);
